75699ca2743427c5c254c95fe0604573588eafa1
shivacheruku10-dev <shivacheruku10@gmail.com>
2026-07-23 21:23:30 +0530

    Add files via upload

---
+++ _2LEDS.ino
@@ -0,0 +1,15 @@
+int g = 10;
+void setup() {
+     pinMode(g,OUTPUT);
+      pinMode(o,OUTPUT);
+}
+
+void loop() {
+     digitalWrite(g,HIGH);
+     delay(2000);
+     digitalWrite(g,LOW);
+     delay(2000);
+  
+  
+
+}
